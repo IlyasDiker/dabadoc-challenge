7aebeb8dea12e84ee5fd6a95a438f5a3b626990b
IlyasDiker <ilyasbenhssine@gmail.com>
2023-01-27 10:06:42 +0100

    add encode and decode jwt

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+    skip_before_action :verify_authenticity_token
+
+    def encode_token(payload)
+        JWT.encode(payload, 'secret')
+    end
+
+    def decode_token
+        auth_header = request.headers['Authorization']
+        if auth_header
+            token = auth_header.split(' ')[1]
+            begin
+                JWT.decode(token, 'secret', true, algorithm: 'HS256')
+            rescue JWT::DecodeError
+                nil
+            end
+        end
+    end
 end
