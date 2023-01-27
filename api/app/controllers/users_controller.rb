94528a0715b2df9c1f2b5a7dd6af7eab1f306639
IlyasDiker <ilyasbenhssine@gmail.com>
2023-01-27 13:38:02 +0100

    update User Model + add login, register endpoints + users_controller

--- api/app/controllers/users_controller.rb
+++ api/app/controllers/users_controller.rb
@@ -1,15 +1,45 @@
 class UsersController < ApplicationController
 
     def login
+        @user = User.find_by(email: login_params[:email])
 
+        p @user.authenticate(login_params[:password])
+        if @user && @user.authenticate(login_params[:password])
+            token = encode_token({ user_id: @user.id})
+            render json: {
+                user: @user,
+                token: token
+            }, status: :ok
+        else 
+            render json: {
+                error: 'Invalid username or password'
+            }, status: :unprocessable_entity
+        end
     end
 
-    def register
-        user = User.new(user_params)
+    def create
+        @user = User.create(user_params)
         
+        if @user.save
+            token = encode_token({ user_id: @user.id})
+            render json: {
+                user: @user,
+                token: token
+            }, status: :ok
+        else 
+            render json: {
+                error: 'Invalid username or password'
+            }, status: :unprocessable_entity
+        end
     end
 
     private
-        def user_params
-            params.require(:user).permit(:name, :email, :password)
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password)
+    end
+
+    def login_params
+        params.require(:user).permit(:email, :password)
+    end
 end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  post '/register' => 'users#register'
-  post '/login' => 'users#login'
+  resource :users, only: [:create]
+  post "/login", to: "users#login"
 end
